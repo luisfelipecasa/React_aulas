5da5a1f2ddea663b189fd7e2e4b4afbb56664032
luisfelipecasa <luisfelipecasagrande878@gmail.com>
2025-09-04 11:22:32 -0300

    editar e excluir

--- src/AppTarefas.jsx
+++ src/AppTarefas.jsx
@@ -14,11 +14,25 @@ const AppTarefas = () => {
         setTarefas([...tarefas, novaTarefa]);
     }
 
+    const editTarefa = (id, novoTexto) => {
+        setTarefas(tarefas.map(
+            tarefa => tarefa.id === id ? {...tarefa, texto: novoTexto} : tarefa
+        ))
+    }
+
+    const deleteTarefa = (id) => {
+        setTarefas(tarefas.filter(tarefa => tarefa.id !== id))
+    }
+
     return (
         <div>
             <h1>☆*: .｡. o(≧▽≦)o .｡.:*☆ Keepão React</h1>
-            <TarefasForm onAddTarefa={addTarefa}/>
-            <TarefasLista tarefas={tarefas}/>
+            <TarefasForm onAddTarefa={addTarefa} />
+            <TarefasLista
+                tarefas={tarefas}
+                onEditTarefa={editTarefa}
+                onDeleteTarefa={deleteTarefa}
+            />
         </div>
     );
 }
